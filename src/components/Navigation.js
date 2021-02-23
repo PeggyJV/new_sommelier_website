9ea5543e6b3603bc3cfb77ac742af5ddbd9c2f4d
ashing <ddguru777@gmail.com>
2021-02-22 20:53:55 -0500

    initial commit & storyblok & global css

---
+++ src/components/Navigation.js
@@ -0,0 +1,48 @@
+import Link from 'gatsby-link'
+import React from "react"
+import SbEditable from 'storyblok-react'
+
+const Nav = ({ settings, lang }) => (
+  <header className="container-fluid">
+    <nav className="" role="navigation">
+      <div className="nav-container d-flex container align-items-center">
+        <div className="text-primary">
+          <Link to="/"> 
+            <p className="nav-title m-0">SOMMELIER</p>
+          </Link>
+        </div>
+        <div className="d-none">
+          <button
+            className=""
+            type="button"
+          >
+            <svg
+              className="h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="d-flex flex-grow-1">
+          <ul className="ml-auto">
+            {settings &&
+              settings.content.main_navi.map((navitem, index) => (
+                <SbEditable content={navitem} key={navitem._uid}>
+                <li key={index}>
+                  <Link to={`/${navitem.link.cached_url.replace("en/", "").replace("home", "")}`} prefetch="true" className="nav-menu-item">
+                    {navitem.name}
+                  </Link>
+                </li>
+                </SbEditable>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+);
+
+export default Nav;
---
+++ src/components/Page.js
@@ -0,0 +1,18 @@
+import React from "react"
+import DynamicComponent from "./DynamicComponent"
+import SbEditable from 'storyblok-react'
+
+const Page = ({ blok }) => {
+  const content =
+    blok.body &&
+    blok.body.map(childBlok => <DynamicComponent blok={childBlok} key={childBlok._uid}/>)
+//   const hasTitle = blok.title.length ? (<h1 className="text-5xl font-bold font-serif text-primary tracking-wide text-center py-8">{ blok.title }</h1>) : null
+  return (
+    <SbEditable content={blok}>
+        {/* { hasTitle } */}
+        { content }
+    </SbEditable>
+  )
+}
+
+export default Page
