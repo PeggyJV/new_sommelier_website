b6237fec5cb5abe9cf7c3ef2e4c49d6996dd972e
ashing <ddguru777@gmail.com>
2021-02-23 22:07:02 -0500

    feat: add usecases

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,11 @@ const cosmosImg = require('/src/assets/images/cosmos.png')
 const etherbridgeImg = require('/src/assets/images/ether-bridge.png')
 const secureImg = require('/src/assets/images/secure.png')
 const automatedImg = require('/src/assets/images/automated.png')
+// Use Cases
+const imperImg = require('/src/assets/images/impermanent.png')
+const rebalancingImg = require('/src/assets/images/rebalancing.png')
+const lowerImg = require('/src/assets/images/lower-ether.png')
+const greaterImg = require('/src/assets/images/greater.png')
 
 export default function Main(){
   return (
@@ -29,7 +34,7 @@ export default function Main(){
           <img src={etheriumImg} alt='frame image' className='mr-2'/>
         </div>
       </div>
-      <div className='main-why mr-5'>
+      <div className='main-why section-container mr-5'>
         <div className='main-top__left-container'>
           <h2 className='text-uppercase'>
             Why <span >Sommelier</span>
@@ -63,6 +68,46 @@ export default function Main(){
               </li>
             </ul>
           </div>
+        </div>
+      </div>
+      <div className='main-usecases section-container'>
+        <div className='main-top__left-container'>
+          <h2 className='text-uppercase'>
+            Use Cases
+          </h2>
+        </div>
+        <div className='main-usecases__content'>
+          <ul>
+            <li>
+              <div>
+                <img src={imperImg} alt='Cosmos Image' />
+                <h3>Impermanent Loss Protection</h3>
+                <p>Automatically rebalance your portfolio when pool performance changes such that Impermanent and unrealized losses require a change in strategy</p>
+              </div>
+              
+            </li>
+            <li>
+              <div>
+                <img src={rebalancingImg} alt='Ether Bridge Image' />
+                <h3>Automated Portfolio Rebalancing</h3>
+                <p>Automatically rebalance portfolio to focus on high yield Liquidity Provider opportunities and exit lower yielding opportunities</p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <img src={lowerImg} alt='Secure Image' />
+                <h3>Lower Ethereum Gas<br />Transaction Fees</h3>
+                <p>Aggregating and batching transactions with Sommelier will yield lower gas prices for LP growing demand to manage pool position and performance</p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <img src={greaterImg} alt='Automated Image' />
+                <h3>Greater Liquidity Momentum</h3>
+                <p>Automatically rebalance your portfolio when pool performance changes such that Impermanent and unrealized losses require a change in strategy</p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
