accccd5cfedb522376a138b2860b5b3e96986eed
taariq <taariq.lewis@gmail.com>
2021-03-08 20:28:02 -0800

    Update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sommelier: The New CoProcessor for Ethereum`,
+    description: `Sommelier is a bet that Ethereum will be a dominant player in the global economy.`,
+    author: `@sommfinance`,
   },
   plugins: [
     {
