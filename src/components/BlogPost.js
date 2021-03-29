f43d1d5c4eb8d18c193c372903db3f71fdea5abd
Jason Jacobs <nerfnerd@gmail.com>
2021-03-29 00:20:06 -0700

    fix bug for social media links. (#20)
    * fix bug for social media links.

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -44,6 +44,22 @@ const BlogPost = ({ blok }) => {
     getWindowDimensions()
   );
 
+  setTimeout(function(){
+    var href = window.location.href;
+    const facebook_url = 'https://www.facebook.com/sharer/sharer.php?u=' + href;
+    const linkedin_url = 'https://www.linkedin.com/shareArticle?mini=true&url=' + href;
+    const twitter_url = 'http://twitter.com/share?url='+ href;
+
+    const fb = document.getElementById('facebook')
+
+
+    if (fb) {
+      document.getElementById('facebook').setAttribute("href", facebook_url);
+      document.getElementById('linkedin').setAttribute("href", linkedin_url);
+      document.getElementById('twitter').setAttribute("href", twitter_url);
+    }
+  }, 1500);
+
 
   const morePosts = blok.allPosts ? blok.allPosts.filter(post => post.content.featured == false) : [];
   const moreWidth = morePosts ? windowDimensions.width * morePosts.length : 0;
