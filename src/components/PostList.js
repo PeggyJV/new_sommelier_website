b299e4752f7ac52cff37905dcdb94c02e598be7a
Jason Jacobs <nerfnerd@gmail.com>
2021-06-02 12:30:21 -0700

    fix ordering on main blog page.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -31,6 +31,7 @@ const PostsList = ({ blok }) => {
   const morePosts = blok.posts.filter(post => post.content && post.content.featured != null && post.content.featured == false)
   const moreWidth = windowDimensions.width * morePosts.length
 
+
   for (const post of morePosts)
   {
     if(post.content.created_at == "") {
@@ -39,14 +40,11 @@ const PostsList = ({ blok }) => {
     } else {
       post['ordering'] = parseInt(post.content.created_at.split(" ")[0].replace(/-/g, ''));
     }
+
+    //console.log(post);
   }
 
-  console.log('***morePosts**');
   morePosts.sort(function(a, b) { return a.ordering - b.ordering });
-  //morePosts.sort(function(a, b){return parseInt(a.content.created_at.split(" ")[0].replace(/-/g, '')) - parseInt(b.content.created_at.split(" ")[0].replace(/-/g, ''))});
-
-  console.log(morePosts);
-console.log('***morePosts**');
 
   return (
     <SbEditable content={blok} key={blok._uid}>
