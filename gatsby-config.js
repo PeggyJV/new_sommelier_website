58ef84bbecfba19dbc32b2096af7abdf8c2a510d
Jason Jacobs <nerfnerd@gmail.com>
2021-06-03 16:12:09 -0700

    Force head build.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,6 @@ module.exports = {
       resolve: `gatsby-plugin-sass`
     },
     {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/blog/*`],
-      },
-    },
-    {
       resolve: 'gatsby-source-storyblok',
       options: {
         accessToken: 'Hx9jd3xuH0cARCTgIBio2Qtt',
@@ -44,6 +38,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `static/images/etherium.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/blog/*`],
       },
     },
     {
