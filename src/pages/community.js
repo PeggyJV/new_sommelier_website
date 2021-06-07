c18b9dd2615bc91bd900c7655d4e80dd389516ae
Jason Jacobs <nerfnerd@gmail.com>
2021-06-07 13:11:35 -0700

    Add community page.

---
+++ src/pages/community.js
@@ -0,0 +1,302 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
+//import "../styles/normalize.css"
+//import "../styles/style.css"
+
+
+export default class extends React.Component {
+  render() {
+
+    return (
+      <>
+        <Helmet>
+          <title>Home</title>
+          <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css" />
+          <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css" />
+          <link rel="stylesheet" href="assets/tether/tether.min.css" />
+          <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+          <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css" />
+          <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css" />
+          <link rel="stylesheet" href="assets/dropdown/css/style.css" />
+          <link rel="stylesheet" href="assets/animatecss/animate.css" />
+          <link rel="stylesheet" href="assets/socicon/css/styles.css" />
+          <link rel="stylesheet" href="assets/theme/css/style.css" />
+          <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"/>
+          <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css" />
+
+          <script src="assets/web/assets/jquery/jquery.min.js"></script>
+          <script src="assets/popper/popper.min.js"></script>
+          <script src="assets/tether/tether.min.js"></script>
+          <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+          <script src="assets/smoothscroll/smooth-scroll.js"></script>
+          <script src="assets/dropdown/js/nav-dropdown.js"></script>
+          <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+          <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+          <script src="assets/viewportchecker/jquery.viewportchecker.js"></script>
+          <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+          <script src="assets/mbr-testimonials-slider/mbr-testimonials-slider.js"></script>
+          <script src="assets/mbr-switch-arrow/mbr-switch-arrow.js"></script>
+          <script src="assets/theme/js/script.js"></script>
+        </Helmet>
+        <div className="bg-gray-300">
+          <Navigation />
+          <main>
+            <section class="header11 cid-sycAsmNe8P" id="header11-1">
+
+            <div class="container-fluid">
+            <div class="row justify-content-center">
+            <div class="col-12 col-md-5 image-wrapper">
+            <a href="https://sommelier.finance/"><img class="w-100" src="assets/images/background4-1.png" alt=""/></a>
+            </div>
+            <div class="col-12 col-md">
+            <div class="text-wrapper text-center">
+            <h1 class="mbr-section-title mbr-fonts-style mb-3 display-2"><strong>Sommelier Upgrades Your Uniswap v3</strong></h1>
+            <p class="mbr-text mbr-fonts-style display-7"><strong>-&gt;</strong> Simplified Uniswap v3 Pairings<br/><strong>&nbsp;-&gt;</strong> Exclusive Market Insights<br/><strong>-&gt;</strong> The Easiest Way to Earn Fees on Your Favorite Tokens</p>
+            <div class="mbr-section-btn mt-3"><a class="btn btn-primary display-7" href="https://t.me/getsomm"><span class="socicon socicon-telegram mbr-iconfont mbr-iconfont-btn"></span>JOIN OUR COMMUNITY</a></div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </section>
+
+            <section class="content6 cid-sycQlZoimH" id="content6-3">
+
+            <div class="container">
+            <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+            <hr class="line"/>
+            <p class="mbr-text align-center mbr-fonts-style my-4 display-5">
+            <em><strong>Enter Sommelier</strong></em><br/><em>The new Coprocessor for Ethereum that moves your DeFi assets into higher yields - faster and cheaper.</em></p>
+            <hr class="line"/>
+            </div>
+            </div>
+            </div>
+            </section>
+
+            <section class="content4 cid-szgK3KUiOE" id="content4-c">
+
+
+            <div class="container">
+            <div class="row justify-content-center">
+            <div class="title col-md-12 col-lg-10">
+            <h3 class="mbr-section-title mbr-fonts-style align-center mb-4 display-5"><strong>THE NEW COPROCESSOR FOR ETHEREUM</strong></h3>
+            <h4 class="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">Sommelier is a bet that Ethereum will be a dominant player in the global economy. Sommelier consists of the Cosmos Stargate SDK, its Tendermint-based consensus layer and a decentralized, bi-directional Ethereum bridge, managed by a global network of validators.
+            <div>
+            </div><div>Liquidity Providers (LPs) will be able to use the Sommelier to author and execute complex, and automated financial transactions, such as portfolio rebalancing, limit orders, batched orders, as well as a host of other features that traders have come to expect from CeFi, but that are not currently available in DeFi.</div></h4>
+            <div class="mbr-section-btn align-center"><a class="btn btn-primary display-7" href="https://t.me/getsomm"><span class="socicon socicon-telegram mbr-iconfont mbr-iconfont-btn"></span>Join Our Community</a> <a class="btn btn-secondary-outline display-7" href="https://app.sommelier.finance/"><span class="mobi-mbri mobi-mbri-to-local-drive mbr-iconfont mbr-iconfont-btn"></span>Launch Application</a></div>
+            </div>
+            </div>
+            </div>
+            </section>
+
+            <section class="testimonails3 carousel slide testimonials-slider cid-syd8ufZUmU" data-interval="false" id="testimonials3-5">
+
+
+
+
+
+            <div class="text-center container">
+            <h3 class="mb-4 mbr-fonts-style display-5"><strong>BENEFITS</strong></h3>
+
+            <div class="carousel slide" role="listbox" data-pause="true" data-keyboard="false" data-ride="carousel" data-interval="8000">
+            <div class="carousel-inner">
+
+
+            <div class="carousel-item">
+            <div class="user col-md-8">
+            <div class="user_image">
+            <img src="assets/images/rebalancing.png" alt=""/>
+            </div>
+            <div class="user_text mb-4">
+            <p class="mbr-fonts-style display-7"><strong>AUTOMATED PORTFOLIO REBALANCING</strong><br/>Automatically rebalance portfolio to focus on high yield Liquidity Provider opportunities and exit lower yielding opportunities.</p>
+            </div>
+
+
+            </div>
+            </div><div class="carousel-item">
+            <div class="user col-md-8">
+            <div class="user_image">
+            <img src="assets/images/greater.png" alt=""/>
+            </div>
+            <div class="user_text mb-4">
+            <p class="mbr-fonts-style display-7"><strong>GREATER LIQUIDITY MOMENTUM</strong><br/>Native transaction batching and roll-ups deliver greater liquidity momentum to capture onchain yield on the Ethereum blockchain.</p>
+            </div>
+
+
+            </div>
+            </div><div class="carousel-item">
+            <div class="user col-md-8">
+            <div class="user_image">
+            <img src="assets/images/impermanent.png" alt=""/>
+            </div>
+            <div class="user_text mb-4">
+            <p class="mbr-fonts-style display-7"><strong>IMPERMANENT LOSS PROTECTION</strong><br/>Automatically rebalance your portfolio when pool performance changes such that Impermanent and unrealized losses require a change in strategy.</p>
+            </div>
+
+
+            </div>
+            </div><div class="carousel-item">
+            <div class="user col-md-8">
+            <div class="user_image">
+            <img src="assets/images/lower-ether.png" alt=""/>
+            </div>
+            <div class="user_text mb-4">
+            <p class="mbr-fonts-style display-7"><strong>LOWER ETHEREUM GAS TRANSACTION FEES</strong><br/>Aggregating and batching transactions with Sommelier will yield lower gas prices for LP growing demand to manage pool position and performance.<br/></p>
+            </div>
+
+
+            </div>
+            </div></div>
+
+            <div class="carousel-controls">
+            <a class="carousel-control-prev" role="button" data-slide="prev">
+            <span aria-hidden="true" class="mobi-mbri mobi-mbri-arrow-prev mbr-iconfont"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+
+            <a class="carousel-control-next" role="button" data-slide="next">
+            <span aria-hidden="true" class="mobi-mbri mobi-mbri-arrow-next mbr-iconfont"></span>
+            <span class="sr-only">Next</span>
+            </a>
+            </div>
+            </div>
+            </div>
+            </section>
+
+            <section class="content16 cid-sydmKNAQAx" id="content16-b">
+
+
+
+
+            <div class="container">
+            <div class="row justify-content-center">
+            <div class="col-12 col-md-10">
+            <div class="mbr-section-head align-center mb-4">
+            <h3 class="mbr-section-title mb-0 mbr-fonts-style display-2"><strong>Pairings by Sommelier: The FAQ</strong></h3>
+
+            </div>
+            <div id="bootstrap-accordion_5" class="panel-group accordionStyles accordion" role="tablist" aria-multiselectable="true">
+            <div class="card mb-3">
+            <div class="card-header" role="tab" id="headingOne">
+            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse1_5" aria-expanded="false" aria-controls="collapse1">
+            <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>WHAT IS PAIRING BY SOMMELIER?</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</h6>
+            <span class="sign mbr-iconfont mbri-arrow-down"></span>
+            </a>
+            </div>
+            <div id="collapse1_5" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_5">
+            <div class="panel-body">
+            <p class="mbr-fonts-style panel-text display-7">Pairings by Sommelier is a platform for liquidity providers for submitting their liquidity positions on Uniswap v3. It automates adding liquidity to token pairs to maximize yields. Sommelier is open-source software licensed under Apache2.</p>
+            </div>
+            </div>
+            </div>
+            <div class="card mb-3">
+            <div class="card-header" role="tab" id="headingOne">
+            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse2_5" aria-expanded="false" aria-controls="collapse2">
+            <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>HOW DO I USE PAIRINGS BY SOMMELIER?&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</strong></h6>
+            <span class="sign mbr-iconfont mbri-arrow-down"></span>
+            </a>
+            </div>
+            <div id="collapse2_5" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_5">
+            <div class="panel-body">
+            <p class="mbr-fonts-style panel-text display-7">To get started you need an Ethereum Wallet and some ETH. After that, navigate to the app, connect your Wallet and use the search tool to select a pair that you want to provide liquidity for. You can see your liquidity range and adjust your transaction speed before adding your liquidity to the pool.
+            <br/>
+            <br/>Each transaction on Sommelier costs ETH known as a “gas fee” that is paid to the miners to keep the network running.</p>
+            </div>
+            </div>
+            </div>
+            <div class="card mb-3">
+            <div class="card-header" role="tab" id="headingOne">
+            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse3_5" aria-expanded="false" aria-controls="collapse3">
+            <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>HOW DOES PAIRINGS RECOMMEND A LIQUIDITY RANGE?</strong></h6>
+            <span class="sign mbr-iconfont mbri-arrow-down"></span>
+            </a>
+            </div>
+            <div id="collapse3_5" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_5">
+            <div class="panel-body">
+            <p class="mbr-fonts-style panel-text display-7">When adding liquidity to a pool in Uniswap v3, Sommelier app displays what prices your liquidity is active over. This is referred to as a liquidity range or tick range.
+            <br/>
+            <br/>Sommelier uses market data from a variety of sources to recommend best yielding liquidity range for the user.</p>
+            </div>
+            </div>
+            </div>
+            <div class="card mb-3">
+            <div class="card-header" role="tab" id="headingOne">
+            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse4_5" aria-expanded="false" aria-controls="collapse4">
+            <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>HOW DO I ADD LIQUIDITY?</strong></h6>
+            <span class="sign mbr-iconfont mbri-arrow-down"></span>
+            </a>
+            </div>
+            <div id="collapse4_5" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_5">
+            <div class="panel-body">
+            <p class="mbr-fonts-style panel-text display-7">After you have connected your wallet, navigate back the search tool to search for a pair that you want to provide liquidity for. You can choose the desired liquidity range, adjust slippage tolerance and transaction speed before adding your liquidity to the pool.
+            <br/>
+            <br/><strong>What is the difference between Standard, Fast and Fastest transaction speeds?
+            </strong><br/>
+            <br/>The faster the chosen transaction speed the higher the fees. Larger fees incentivize miners to include a user’s transaction in a block more quickly. Until a miner includes a users transaction, their users liquidity position will not become active in trades.
+            <br/>
+            <br/><strong>What is the difference between Bearish, Neutral and Bullish market sentiment?
+            </strong><br/>
+            <br/>Market sentiment is the tone of a market, indicated through price movement. Market sentiment is bullish when prices are rising. Market sentiment is bearish when prices are falling.
+            <br/>
+            <br/>When a user selects bullish or bearish price sentiment, the recommended liquidity range is adjusted by widening the range in the direction of market prices.</p>
+            </div>
+            </div>
+            </div>
+            <div class="card mb-3">
+            <div class="card-header" role="tab" id="headingOne">
+            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse5_5" aria-expanded="false" aria-controls="collapse5">
+            <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>WHAT IS SLIPPAGE TOLERANCE?</strong></h6>
+            <span class="sign mbr-iconfont mbri-arrow-down"></span>
+            </a>
+            </div>
+            <div id="collapse5_5" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_5">
+            <div class="panel-body">
+            <p class="mbr-fonts-style panel-text display-7">Slippage in trading occurs when the price at which the order gets executed does not match the price at the time when you confirmed the transaction. When trading on Uniswap, this is referred to as “slippage tolerance” and is expressed as a percentage.
+            <br/>
+            <br/>Sommelier is calculating suggested slippage tolerance for you based on historical data on what price other users expect to get vs what they actually got due to other trades executing first.
+            <br/>
+            <br/>You can adjust your slippage tolerance percentage under the settings before submitting your transaction through Sommelier app.</p>
+            </div>
+            </div>
+            </div>
+
+            </div>
+            </div>
+            </div>
+            </div>
+            </section>
+
+            <section class="gallery1 cid-sydfYKOzHp" id="gallery1-9">
+
+
+            <div class="container">
+            <div class="mbr-section-head">
+            <h4 class="mbr-section-title mbr-fonts-style align-center mb-0 display-5"><strong>BACKED BY</strong></h4>
+
+            </div>
+            <div class="row mt-4">
+
+            <div class="item features-image сol-12 col-md-6 col-lg-12 active">
+            <div class="item-wrapper">
+            <div class="item-img">
+            <img src="assets/images/untitled-1.png" data-slide-to="0" alt=""/>
+            </div>
+
+
+            </div>
+            </div>
+
+
+            </div>
+            </div>
+            </section>
+
+          </main>
+          <Footer />
+        </div>
+      </>
+    )
+  }
+}
