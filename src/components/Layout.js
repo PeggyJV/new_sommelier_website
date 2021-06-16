068e874fc06e7dc5180a74f68150995d54f2a82f
Jason Jacobs <nerfnerd@gmail.com>
2021-06-16 14:53:07 -0700

    move mixpanel to core.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -36,52 +36,56 @@ export default function Layout({ children, location, lang }){
     origin = window.location.origin;
   }
 
-  let mixpanel = Mixpanel.init('d6a6208c71b46a6965913df792f505f9');
-
-
-  function getQueryParam(url, param) {
-    // Expects a raw URL
-    param = param.replace(/[[]/, "\[").replace(/[]]/, "\]");
-    var regexS = "[\?&]" + param + "=([^&#]*)",
-    regex = new RegExp( regexS ),
-    results = regex.exec(url);
-    if (results === null || (results && typeof(results[1]) !== 'string' && results[1].length)) {
-      return '';
-    } else {
-      return decodeURIComponent(results[1]).replace(/\W/gi, ' ');
-    }
-  };
-  function campaignParams() {
-    var campaign_keywords = 'utm_source utm_medium utm_campaign utm_content utm_term'.split(' ')
-    , kw = ''
-    , params = {}
-    , first_params = {};
-    var index;
-    for (index = 0; index < campaign_keywords.length; ++index) {
-      kw = getQueryParam(document.URL, campaign_keywords[index]);
-      if (kw.length) {
-        params[campaign_keywords[index] + ' [last touch]'] = kw;
-      }
-    }
-    for (index = 0; index < campaign_keywords.length; ++index) {
-      kw = getQueryParam(document.URL, campaign_keywords[index]);
-      if (kw.length) {
-        first_params[campaign_keywords[index] + ' [first touch]'] = kw;
-      }
-    }
-    mixpanel.people.set(params);
-    mixpanel.people.set_once(first_params);
-    //mixpanel.register(params);
-  }
-
-
-  mixpanel.people.set({ 'last touch':  location.href });
-  mixpanel.people.set_once({ 'first touch':  location.href });
-  mixpanel.people.increment('page_views');
-
-  campaignParams();
-
-  mixpanel.track('page_load', { current_url: location.href });
+//   let mixpanel = Mixpanel.init('d6a6208c71b46a6965913df792f505f9');
+//
+//
+//   function getQueryParam(url, param) {
+//     // Expects a raw URL
+//     param = param.replace(/[[]/, "\[").replace(/[]]/, "\]");
+//     var regexS = "[\?&]" + param + "=([^&#]*)",
+//     regex = new RegExp( regexS ),
+//     results = regex.exec(url);
+//     if (results === null || (results && typeof(results[1]) !== 'string' && results[1].length)) {
+//       return '';
+//     } else {
+//       return decodeURIComponent(results[1]).replace(/\W/gi, ' ');
+//     }
+//   };
+//   function campaignParams() {
+//     var campaign_keywords = 'utm_source utm_medium utm_campaign utm_content utm_term'.split(' ')
+//     , kw = ''
+//     , params = {}
+//     , first_params = {};
+//     var index;
+//     for (index = 0; index < campaign_keywords.length; ++index) {
+//       kw = getQueryParam(document.URL, campaign_keywords[index]);
+//       if (kw.length) {
+//         params[campaign_keywords[index] + ' [last touch]'] = kw;
+//       }
+//     }
+//     for (index = 0; index < campaign_keywords.length; ++index) {
+//       kw = getQueryParam(document.URL, campaign_keywords[index]);
+//       if (kw.length) {
+//         first_params[campaign_keywords[index] + ' [first touch]'] = kw;
+//       }
+//     }
+//     mixpanel.people.set(params);
+//     mixpanel.people.set_once(first_params);
+//     //mixpanel.register(params);
+//   }
+//
+// console.log(location);
+//   mixpanel.people.set({ 'last touch':  location.href });
+//   mixpanel.people.set_once({ 'first touch':  location.href });
+//   mixpanel.people.increment('page_views');
+//
+//   campaignParams();
+//
+//
+//   mixpanel.people.set({ 'last touch':  location.href });
+//   mixpanel.people.set_once({ 'first touch':  location.href });
+//   mixpanel.people.increment('page_views');
+//   mixpanel.track('page_load', { current_url: location.href });
 
   return (
     <div className="bg-gray-300">
--- src/html.js
+++ src/html.js
@@ -2,8 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 
 let twitter = "!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('init','o65mg');twq('track','PageView');";
-let mixpnljs = "var _mtm = window._mtm = window._mtm || [];_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.type='text/javascript'; g.async=true; g.src='https://cdn.matomo.cloud/leanmarketing.matomo.cloud/container_zuHO1C32.js'; s.parentNode.insertBefore(g,s);";
-// let mixpnl = `function getQueryParam(url, param) { param = param.replace(/[[]/, "\[").replace(/[]]/, "\]"); var regexS = "[\?&]" + param + "=([^&#]*)", regex = new RegExp( regexS ), results = regex.exec(url); if (results === null || (results && typeof(results[1]) !== 'string' && results[1].length)) { return ''; } else { return decodeURIComponent(results[1]).replace(/\W/gi, ' '); } }; function campaignParams() {var campaign_keywords = 'utm_source utm_medium utm_campaign utm_content utm_term'.split(' '), kw = '', params = {}, first_params = {}; var index; for (index = 0; index < campaign_keywords.length; ++index) { kw = getQueryParam(document.URL, campaign_keywords[index]); if (kw.length) { params[campaign_keywords[index] + ' [last touch]'] = kw; } } for (index = 0; index < campaign_keywords.length; ++index) { kw = getQueryParam(document.URL, campaign_keywords[index]); if (kw.length) { first_params[campaign_keywords[index] + ' [first touch]'] = kw; } } mixpanel.people.set(params); mixpanel.people.set_once(first_params); mixpanel.register(params); } campaignParams();`
+let mtm = "var _mtm = window._mtm = window._mtm || [];_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.type='text/javascript'; g.async=true; g.src='https://cdn.matomo.cloud/leanmarketing.matomo.cloud/container_zuHO1C32.js'; s.parentNode.insertBefore(g,s);";
+
+
+let mixpnl = `function getQueryParam(url, param) { param = param.replace(/[[]/, "\[").replace(/[]]/, "\]"); var regexS = "[\?&]" + param + "=([^&#]*)", regex = new RegExp( regexS ), results = regex.exec(url); if (results === null || (results && typeof(results[1]) !== 'string' && results[1].length)) { return ''; } else { return decodeURIComponent(results[1]).replace(/\W/gi, ' '); } }; function campaignParams() {var campaign_keywords = 'utm_source utm_medium utm_campaign utm_content utm_term'.split(' '), kw = '', params = {}, first_params = {}; var index; for (index = 0; index < campaign_keywords.length; ++index) { kw = getQueryParam(document.URL, campaign_keywords[index]); if (kw.length) { params[campaign_keywords[index] + ' [last touch]'] = kw; } } for (index = 0; index < campaign_keywords.length; ++index) { kw = getQueryParam(document.URL, campaign_keywords[index]); if (kw.length) { first_params[campaign_keywords[index] + ' [first touch]'] = kw; } } mixpanel.people.set(params); mixpanel.people.set_once(first_params); mixpanel.register(params); mixpanel.people.set({ 'last touch':  location.href });mixpanel.people.set_once({ 'first touch':  location.href });mixpanel.people.increment('page_views');mixpanel.track('page_load', { current_url: location.href }); } campaignParams();`
 
 export default function HTML(props) {
   return (
@@ -16,11 +18,15 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js" async defer></script>
+        <script type="text/javascript" src="/mp.js" ></script>
         <script
-          dangerouslySetInnerHTML={{ __html: mixpnljs }}
+          dangerouslySetInnerHTML={{ __html: mtm }}
         />
         <script
           dangerouslySetInnerHTML={{ __html: twitter }}
+        />
+        <script
+          dangerouslySetInnerHTML={{ __html: mixpnl }}
         />
         {props.headComponents}
       </head>
