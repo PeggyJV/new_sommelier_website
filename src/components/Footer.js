90e3f39b34591f5f33013c314d0f55ceda3535d0
Jason Jacobs <nerfnerd@gmail.com>
2021-06-17 10:06:38 -0700

    Add disclaimer.. save page visited to dollar global.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,6 +18,7 @@ const Footer = () => {
           <a href='https://discord.gg/ZcAYgSBxvY' className='ml-4' target="_blank"><img src='/images/ico-twitter.png' alt='Twitter' width='24' /></a>
         </div>
       </div>
+      <br/><br/>Sommelier is alpha software. Sommelier is non-custodial and has no warranties. We also do not endorse any pools that we share on our channels. Pools may be compromised and things on Sommelier break all the time and you may lose your money. We are not giving you investment advice with this update and Sommelier does not control your funds. Again, all our software is alpha and undergoing daily updates and things can break all the time.<br/><br/>
     </footer>
     </div>
   )
