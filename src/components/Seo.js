2a56c7a0508a970852db36fda29cfb17fff08c10
andreaangelova <andrea.angelova@hotmail.com>
2021-07-18 12:48:58 +0200

    adding only one header and one title on all pages

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -5,37 +5,38 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
-
-function SEO({ description, title, image, content }) {
-
-
-  const metaDescription = "Sommelier is the new Coprocessor for the Ethereum VM";
-  const defaultTitle = "Sommelier";
-
-
-  let metaImage = image;
-
-
-  return (
-    <head>
-      <title>{defaultTitle}</title>
-      <meta name="description" content={metaDescription} />
-      {metaImage && <meta name="image" content={metaImage} />}
-
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={defaultTitle} />
-      <meta property="og:description" content={metaDescription} />
-      {metaImage && <meta property="og:image" content={metaImage} />}
-
-      <meta name="twitter:title" content={defaultTitle} />
-      <meta name="twitter:description" content={description} />
-      {metaImage && <meta name="twitter:image" content={metaImage} />}
-    </head>
-  )
-}
-
-export default SEO
+ import React from "react"
+ import PropTypes from "prop-types"
+ import { Helmet } from "react-helmet"
+ import { useStaticQuery, graphql } from "gatsby"
+ 
+ function SEO({ description, title, image, content }) {
+ 
+ 
+   const metaDescription = "Sommelier is the new Coprocessor for the Ethereum VM";
+   const defaultTitle = "Sommelier";
+ 
+ 
+   let metaImage = image;
+ 
+ 
+   return (
+     <Helmet>
+       <title>{defaultTitle}</title>
+       <meta name="description" content={metaDescription} />
+       {metaImage && <meta name="image" content={metaImage} />}
+ 
+       <meta property="og:type" content="website" />
+       <meta property="og:title" content={defaultTitle} />
+       <meta property="og:description" content={metaDescription} /> 
+       {metaImage && <meta property="og:image" content={metaImage} />}
+ 
+       <meta name="twitter:title" content={defaultTitle} />
+       <meta name="twitter:description" content={description} />
+       {metaImage && <meta name="twitter:image" content={metaImage} />}
+     </Helmet>
+   )
+ }
+ 
+ export default SEO
+ 
