472a3a392085234b2cc833faf409c741588e2ec5
Zaki Manian <zaki@iqlusion.io>
2022-03-01 16:26:09 -0800

    Handle SSR in another code path

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,6 +10,7 @@ import SEO from "../components/HeadSeo"
 import "../assets/scss/blog.scss"
 
 setTimeout(function(){
+  try {
   var href = window.location.href;
   const facebook_url = 'https://www.facebook.com/sharer/sharer.php?u=' + href;
   const linkedin_url = 'https://www.linkedin.com/shareArticle?mini=true&url=' + href;
@@ -23,6 +24,10 @@ setTimeout(function(){
     document.getElementById('linkedin').setAttribute("href", linkedin_url);
     document.getElementById('twitter').setAttribute("href", twitter_url);
   }
+} catch (err) {
+  console.error(err);
+}
+
 }, 1500);
 
 export default class extends React.Component {
